1d39dc710f984461e9ddb12b4e7314900f9b8fd1
FasilDev <144022305+FasilDev@users.noreply.github.com>
2025-07-01 13:12:33 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,16 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
+# Create media directory
+mkdir -p media
+mkdir -p media/profile_pics
+
+# Set permissions for media directory
+chmod 755 media
+chmod 755 media/profile_pics
+
 # Collect static files
 python manage.py collectstatic --no-input
 
 # Run migrations
-python manage.py migrate 
+python manage.py migrate
